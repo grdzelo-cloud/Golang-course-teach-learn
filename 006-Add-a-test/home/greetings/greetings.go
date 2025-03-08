f55bf0fe422c15e4eaf065dd0f3e360b93e65295
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2025-03-09 01:32:54 +0400

    finish: basic

---
+++ 006-Add-a-test/home/greetings/greetings.go
@@ -0,0 +1,47 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty string")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages
+
+	messages := make(map[string]string)
+
+	// Loop thought the received slice of names, calling
+	// the Hello function to get a message for each name.
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// If the map. associate the retrieved message with
+		// the name
+		messages[name] = message
+	}
+	return messages, nil
+
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Zdarova %v. Dzma!",
+		"Shen %v, ar xar?!",
+		"Ra ubneli xar %v",
+	}
+	return formats[rand.Intn(len(formats))]
+}
